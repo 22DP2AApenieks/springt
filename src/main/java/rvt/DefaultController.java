887a931af3bc553544e553b8d40dfffc47948e8b
22DP2AApenieks <131154370+22DP2AApenieks@users.noreply.github.com>
2023-12-14 13:41:32 +0000

    uzl

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
@@ -12,4 +13,15 @@ public class DefaultController {
     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
         return "index";
     }
+    @GetMapping(value = "/students")
+    public ModelAndView students(){
+        ModelAndView modelAndView = new ModelAndView("Students");
+        return modelAndView;
+    }
+    @GetMapping(value = "/about")
+    public ModelAndView about(){
+        ModelAndView modelAndView = new ModelAndView("about");
+        return modelAndView;
+    }
+    
 }
